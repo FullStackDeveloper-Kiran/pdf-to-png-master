216fb05422ff30e5779a1e1147f4c6bfde67f4bb
Kiran M <94104935+FullStackDeveloper-Kiran@users.noreply.github.com>
2022-03-26 11:22:07 +0530

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'convertapi/convertapi-php' => array(
+            'pretty_version' => 'v1.5.0',
+            'version' => '1.5.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../convertapi/convertapi-php',
+            'aliases' => array(),
+            'reference' => '89d1600a19b3e541fd93abfe6788d8881f84ac6a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
